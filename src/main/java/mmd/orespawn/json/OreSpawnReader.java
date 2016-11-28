71e84e1ade2e847185d81bd41e2a38b27e98a7ab
iLexiconn <ilexiconn@gmail.com>
2016-11-28 20:14:48 +0100

    Add /addore and /dumpbiomes Remove legacy json converter Fix /help command

--- src/main/java/mmd/orespawn/json/OreSpawnReader.java
+++ src/main/java/mmd/orespawn/json/OreSpawnReader.java
@@ -5,6 +5,7 @@ import mmd.orespawn.OreSpawn;
 import mmd.orespawn.api.DimensionLogic;
 import mmd.orespawn.api.OreSpawnAPI;
 import mmd.orespawn.api.SpawnLogic;
+import mmd.orespawn.util.StateUtil;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.crash.CrashReport;
@@ -68,23 +69,9 @@ public enum OreSpawnReader {
 
                         if (ore.has("state")) {
                             String stateString = ore.get("state").getAsString();
-                            boolean foundState = false;
+                            state = StateUtil.deserializeState(block, stateString);
 
-                            for (IBlockState validState : block.getBlockState().getValidStates()) {
-                                String string = validState.toString();
-                                string = string.substring(string.indexOf("[") + 1, string.length() - (string.endsWith("]") ? 1 : 0));
-                                if (string.equals(block.getRegistryName().toString())) {
-                                    string = "";
-                                }
-
-                                if (stateString.equals(string)) {
-                                    state = validState;
-                                    foundState = true;
-                                    break;
-                                }
-                            }
-
-                            if (!foundState) {
+                            if (state == null) {
                                 throw new RuntimeException("Invalid state " + stateString + " for block " + block.getRegistryName());
                             }
                         }
@@ -119,107 +106,5 @@ public enum OreSpawnReader {
                 OreSpawn.LOGGER.info(report.getCompleteReport());
             }
         });
-    }
-
-    @Deprecated //planned for removal in 2.1.0
-    public void convertOldSpawnEntries() {
-        File directory = new File(".", "config" + File.separator + "orespawn");
-        JsonParser parser = new JsonParser();
-
-        if (!directory.exists() || !directory.isDirectory()) {
-            return;
-        }
-
-        File[] files = directory.listFiles();
-
-        if (files == null) {
-            return;
-        }
-
-        Arrays.stream(files).filter(file -> file.getName().endsWith(".json")).forEach(file -> {
-            try {
-                OreSpawn.LOGGER.info("Converting JSON " + file.getName());
-
-                JsonElement element = parser.parse(FileUtils.readFileToString(file));
-                JsonObject object = element.getAsJsonObject();
-                JsonArray dimensions = object.get("dimensions").getAsJsonArray();
-
-                SpawnLogic spawnLogic = OreSpawn.API.createSpawnLogic();
-
-                for (JsonElement dimensionsEntry : dimensions) {
-                    JsonObject dimension = dimensionsEntry.getAsJsonObject();
-
-                    int newDimension = OreSpawnAPI.DIMENSION_WILDCARD;
-
-                    JsonPrimitive id = dimension.get("dimension").getAsJsonPrimitive();
-                    if (id.isNumber()) {
-                        newDimension = id.getAsInt();
-                    }
-
-                    DimensionLogic dimensionLogic = spawnLogic.getDimension(newDimension);
-
-                    JsonArray ores = dimension.get("ores").getAsJsonArray();
-
-                    for (JsonElement oreEntry : ores) {
-                        JsonObject ore = oreEntry.getAsJsonObject();
-
-                        Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(ore.get("blockID").getAsString()));
-
-                        if (block == null) {
-                            continue;
-                        }
-
-                        IBlockState state = block.getDefaultState();
-
-                        if (ore.has("blockMeta")) {
-                            state = block.getStateFromMeta(ore.get("blockMeta").getAsInt());
-                        }
-
-                        int size = ore.get("size").getAsInt();
-                        int variation = ore.get("variation").getAsInt();
-                        int frequency = ore.get("frequency").getAsInt();
-                        int minHeight = ore.get("minHeight").getAsInt();
-                        int maxHeight = ore.get("maxHeight").getAsInt();
-                        List<Biome> biomes = new ArrayList<>();
-
-                        if (ore.has("biomes")) {
-                            JsonArray biomesArray = ore.get("biomes").getAsJsonArray();
-
-                            for (JsonElement biomeEntry : biomesArray) {
-                                String biome = biomeEntry.getAsString();
-
-                                try {
-                                    int biomeID = Integer.parseInt(biome);
-                                    Biome result = Biome.getBiome(biomeID);
-
-                                    if (result != null) {
-                                        biomes.add(result);
-                                    }
-                                } catch (NumberFormatException e) {
-                                    for (Biome result : ForgeRegistries.BIOMES) {
-                                        if (result.getBiomeName().equals(biome)) {
-                                            biomes.add(result);
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                        dimensionLogic.addOre(state, size, variation, frequency, minHeight, maxHeight, biomes.toArray(new Biome[biomes.size()]));
-                    }
-
-                }
-
-                OreSpawn.API.registerSpawnLogic(file.getName().substring(0, file.getName().lastIndexOf(".")), spawnLogic);
-                file.delete();
-            } catch (Exception e) {
-                CrashReport report = CrashReport.makeCrashReport(e, "Failed reading config " + file.getName());
-                report.getCategory().addCrashSection("OreSpawn Version", OreSpawn.VERSION);
-                OreSpawn.LOGGER.info(report.getCompleteReport());
-            }
-        });
-
-        directory.delete();
     }
 }
