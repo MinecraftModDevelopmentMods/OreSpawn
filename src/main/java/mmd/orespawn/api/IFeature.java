70c218abfe3129a46b0328d05f268edf0f3057b5
Daniel Hazelton <dshadowwolf@gmail.com>
2017-06-21 15:32:35 -0500

    Add a temporary compat namespace so mods relying on the OS2 API being in the mmd.orespawn.api package don't crash if they try to interface with OS3

---
+++ src/main/java/mmd/orespawn/api/IFeature.java
@@ -0,0 +1,14 @@
+package mmd.orespawn.api;
+
+import java.util.Random;
+
+import com.mcmoddev.orespawn.data.DefaultOregenParameters;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkGenerator;
+import net.minecraft.world.chunk.IChunkProvider;
+
+public interface IFeature {
+	void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
+			IChunkProvider chunkProvider, DefaultOregenParameters p);
+}
