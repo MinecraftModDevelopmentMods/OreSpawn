6e65c59770c0aab899a794a7b1a9cc1ba0d9803d
Daniel Hazelton <dshadowwolf@gmail.com>
2017-07-04 22:02:47 -0400

    the 'FunctionalInterface' annotation on the plugin class interface isn't needed, as per @pau101's review

--- src/main/java/com/mcmoddev/orespawn/api/plugin/IOreSpawnPlugin.java
+++ src/main/java/com/mcmoddev/orespawn/api/plugin/IOreSpawnPlugin.java
@@ -2,7 +2,6 @@ package com.mcmoddev.orespawn.api.plugin;
 
 import com.mcmoddev.orespawn.api.os3.OS3API;
 
-@FunctionalInterface
 public interface IOreSpawnPlugin {
 	void register(OS3API apiInterface);
 }
