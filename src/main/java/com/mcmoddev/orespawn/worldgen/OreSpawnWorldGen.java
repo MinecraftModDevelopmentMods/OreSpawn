4a46b8b04c6c1b1a335439b0e38ed4f8f1cfaf17
Daniel Hazelton <dshadowwolf@gmail.com>
2017-07-13 19:08:54 -0400

    port to 1.12

--- src/main/java/com/mcmoddev/orespawn/worldgen/OreSpawnWorldGen.java
+++ src/main/java/com/mcmoddev/orespawn/worldgen/OreSpawnWorldGen.java
@@ -21,7 +21,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.chunk.IChunkGenerator;
+import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraftforge.fml.common.IWorldGenerator;
 import net.minecraftforge.oredict.OreDictionary;
