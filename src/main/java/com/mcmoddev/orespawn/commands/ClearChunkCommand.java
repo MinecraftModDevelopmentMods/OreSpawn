8354e21c8abd897fe70b818f21d3b4bf8f5bb471
Daniel Hazelton <dshadowwolf@gmail.com>
2017-10-28 18:17:59 -0400

    extend the clearchunk command to be more useful

--- src/main/java/com/mcmoddev/orespawn/commands/ClearChunkCommand.java
+++ src/main/java/com/mcmoddev/orespawn/commands/ClearChunkCommand.java
@@ -1,5 +1,9 @@
 package com.mcmoddev.orespawn.commands;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.mcmoddev.orespawn.worldgen.OreSpawnWorldGen;
 import net.minecraft.block.Block;
 import net.minecraft.command.CommandBase;
@@ -8,10 +12,12 @@ import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class ClearChunkCommand extends CommandBase {
     @Override
@@ -21,7 +27,7 @@ public class ClearChunkCommand extends CommandBase {
 
     @Override
     public String getUsage(ICommandSender sender) {
-        return "/clearchunk";
+        return "/clearchunk <viewores|dirtandgravel|classic>";
     }
 
     @Override
@@ -33,18 +39,52 @@ public class ClearChunkCommand extends CommandBase {
         EntityPlayer player = (EntityPlayer) sender;
         Chunk chunk = player.getEntityWorld().getChunkFromBlockCoords(player.getPosition());
         ChunkPos chunkPos = chunk.getPos();
+        List<Block> blocks;
+        
+        boolean flagClassic = false;
+        
+        if( args.length > 0 ) {
+    		List<String> blockNames;
+        	switch( args[0].toLowerCase() ) {
+        	case "viewores":
+        		blockNames = Arrays.asList("minecraft:stone", "minecraft:diorite", "minecraft:andesite", "minecraft:granite", "minecraft:dirt",
+        								   "minecraft:grass", "minecraft:gravel", "minecraft:water", "minecraft:lava", "minecraft:sandstone",
+        								   "minecraft:red_sandstone", "minecraft:sand");
+        		break;
+        	case "dirtandgravel":
+        		blockNames = Arrays.asList("minecraft:stone", "minecraft:gravel", "minecraft:dirt", "minecraft:grass", "minecraft:water", "minecraft:lava");
+        		break;
+        	case "classic":
+        		blockNames = OreSpawnWorldGen.getSpawnBlocks().stream().map( block -> block.getRegistryName().toString() ).collect(Collectors.toList());
+        		blockNames.add("minecraft:water");
+        		blockNames.add("minecraft:lava");
+        		flagClassic = true;
+        		break;
+        	default:
+        		blockNames = Arrays.asList("minecraft:stone", "minecraft:water", "minecraft:lava");
+        	}
+        	blocks = blockNames.stream().map( blockName -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName))).collect(Collectors.toList());
+        } else {
+        	blocks = Arrays.asList("minecraft:stone", "minecraft:water", "minecraft:lava").stream()
+        		  .map( blockName -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName))).collect(Collectors.toList());
+        }
+        
+        List<Block> overburden = Arrays.asList("minecraft:dirt", "minecraft:sand", "minecraft:gravel", "minecraft:grass", "minecraft:sandstone", "minecraft:red_sandstone").stream()
+      		  .map( blockName -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName))).collect(Collectors.toList());        		  
 
         for (int x = chunkPos.getXStart(); x <= chunkPos.getXEnd(); x++) {
-            for (int y = 0; y < 256; y++) {
+            for (int y = 256; y >= 0; y--) {
                 for (int z = chunkPos.getZStart(); z <= chunkPos.getZEnd(); z++) {
                     BlockPos pos = new BlockPos(x, y, z);
                     Block block = player.world.getBlockState(pos).getBlock();
-                    if (OreSpawnWorldGen.getSpawnBlocks().contains(block)) {
+                    if (blocks.contains(block) || ((y >= 64 && overburden.contains(block) ) && !flagClassic)) {
                         player.world.setBlockToAir(pos);
                     }
                 }
             }
         }
+        if( flagClassic ) flagClassic = false;
+        
         player.sendStatusMessage(new TextComponentString("chunk "+chunkPos.toString()+" cleared"), true);
     }
 
