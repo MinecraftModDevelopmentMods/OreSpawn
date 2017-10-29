da30840b50f6bac9190cd30859441882b09de866
Daniel Hazelton <dshadowwolf@gmail.com>
2017-10-28 20:30:21 -0400

    cleanup the code here so it isn't an unholy mess

--- src/main/java/com/mcmoddev/orespawn/commands/ClearChunkCommand.java
+++ src/main/java/com/mcmoddev/orespawn/commands/ClearChunkCommand.java
@@ -1,6 +1,7 @@
 package com.mcmoddev.orespawn.commands;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +21,13 @@ import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class ClearChunkCommand extends CommandBase {
+	private static final String STONE_ID = "minecraft:stone";
+	private static final List<String> stoneVariants = Arrays.asList(STONE_ID, "minecraft:diorite", "minecraft:andesite", "minecraft:granite", "minecraft:sandstone", "minecraft:red_sandstone", "minecraft:netherrack", "minecraft:endstone");
+	private static final List<String> baseStones = Arrays.asList(STONE_ID, "minecraft:netherrack", "minecraft:endstone");
+	private static final List<String> dirtVariants = Arrays.asList("minecraft:dirt", "minecraft:grass");
+	private static final List<String> fluidVariants = Arrays.asList("minecraft:water", "minecraft:lava");
+	private static final List<String> otherVariants = Arrays.asList("minecraft:gravel", "minecraft:sand");
+	
     @Override
     public String getName() {
         return "clearchunk";
@@ -41,37 +49,24 @@ public class ClearChunkCommand extends CommandBase {
         ChunkPos chunkPos = chunk.getPos();
         List<Block> blocks;
         
-        boolean flagClassic = false;
+        boolean flagClassic = args.length>0?args[0].toLowerCase().equalsIgnoreCase("classic"):false;
         
-        if( args.length > 0 ) {
-    		List<String> blockNames;
-        	switch( args[0].toLowerCase() ) {
-        	case "viewores":
-        		blockNames = Arrays.asList("minecraft:stone", "minecraft:diorite", "minecraft:andesite", "minecraft:granite", "minecraft:dirt",
-        								   "minecraft:grass", "minecraft:gravel", "minecraft:water", "minecraft:lava", "minecraft:sandstone",
-        								   "minecraft:red_sandstone", "minecraft:sand");
-        		break;
-        	case "dirtandgravel":
-        		blockNames = Arrays.asList("minecraft:stone", "minecraft:gravel", "minecraft:dirt", "minecraft:grass", "minecraft:water", "minecraft:lava");
-        		break;
-        	case "classic":
-        		blockNames = OreSpawnWorldGen.getSpawnBlocks().stream().map( block -> block.getRegistryName().toString() ).collect(Collectors.toList());
-        		blockNames.add("minecraft:water");
-        		blockNames.add("minecraft:lava");
-        		flagClassic = true;
-        		break;
-        	default:
-        		blockNames = Arrays.asList("minecraft:stone", "minecraft:water", "minecraft:lava");
-        	}
-        	blocks = blockNames.stream().map( blockName -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName))).collect(Collectors.toList());
-        } else {
-        	blocks = Arrays.asList("minecraft:stone", "minecraft:water", "minecraft:lava").stream()
-        		  .map( blockName -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName))).collect(Collectors.toList());
-        }
+		List<String> blockNames = new LinkedList<>();
+		blockNames.addAll(fluidVariants);
+		getBlocks(args,blockNames);
+		
+    	blocks = blockNames.stream()
+      		  .map( blockName -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName))).collect(Collectors.toList());
         
         List<Block> overburden = Arrays.asList("minecraft:dirt", "minecraft:sand", "minecraft:gravel", "minecraft:grass", "minecraft:sandstone", "minecraft:red_sandstone").stream()
       		  .map( blockName -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName))).collect(Collectors.toList());        		  
 
+        clearBlocks( chunkPos, blocks, overburden, flagClassic, player);
+        
+        player.sendStatusMessage(new TextComponentString("chunk "+chunkPos.toString()+" cleared"), true);
+    }
+
+    private void clearBlocks(ChunkPos chunkPos, List<Block> blocks, List<Block> overburden, boolean flagClassic, EntityPlayer player) {
         for (int x = chunkPos.getXStart(); x <= chunkPos.getXEnd(); x++) {
             for (int y = 256; y >= 0; y--) {
                 for (int z = chunkPos.getZStart(); z <= chunkPos.getZEnd(); z++) {
@@ -83,12 +78,33 @@ public class ClearChunkCommand extends CommandBase {
                 }
             }
         }
-        if( flagClassic ) flagClassic = false;
-        
-        player.sendStatusMessage(new TextComponentString("chunk "+chunkPos.toString()+" cleared"), true);
-    }
+	}
 
-    @Override
+	private void getBlocks(String[] args, List<String> blockNames) {
+        if( args.length > 0 ) {
+        	switch( args[0].toLowerCase() ) {
+        	case "viewores":
+        		blockNames.addAll(stoneVariants);
+        		blockNames.addAll(dirtVariants);
+        		blockNames.addAll(otherVariants);
+        		break;
+        	case "dirtandgravel":
+        		blockNames.add("minecraft:stone");
+        		blockNames.addAll(dirtVariants);
+        		blockNames.addAll(otherVariants);
+        		break;
+        	case "classic":
+        		blockNames.addAll(OreSpawnWorldGen.getSpawnBlocks().stream().map( block -> block.getRegistryName().toString() ).collect(Collectors.toList()));
+        		break;
+        	default:
+        		blockNames.addAll(baseStones);
+        	}
+        } else {
+        	blockNames.addAll(baseStones);
+        }
+	}
+
+	@Override
     public int compareTo(ICommand command) {
         return this.getName().compareTo(command.getName());
     }
