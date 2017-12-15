9d81250da7b997f2e998da93b2ccf9a2547bc547
Jasmine Iwanek <jasmine@iwanek.co.uk>
2017-12-14 21:36:58 -0800

    fix

--- src/main/java/com/mcmoddev/orespawn/OreSpawn.java
+++ src/main/java/com/mcmoddev/orespawn/OreSpawn.java
@@ -26,6 +26,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent; 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
