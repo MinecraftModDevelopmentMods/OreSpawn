aaade0ac12a373400da728c4ff6f8528c1e33e4c
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-01-03 00:42:23 -0800

    fix logger

--- src/main/java/com/mcmoddev/orespawn/OreSpawn.java
+++ src/main/java/com/mcmoddev/orespawn/OreSpawn.java
@@ -65,7 +65,7 @@ public class OreSpawn {
 
 	@EventHandler
 	public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
-		logger.warn("Invalid fingerprint detected!");
+		LOGGER.warn("Invalid fingerprint detected!");
 	}
 
 	@EventHandler
