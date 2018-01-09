fb93c63e33b4e8ab359d0bdb29f760512cdf1441
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-01-09 03:27:15 -0800

    Orespawn 3.2.1 release
    Also fix Jar Signing

--- src/main/java/com/mcmoddev/orespawn/data/Constants.java
+++ src/main/java/com/mcmoddev/orespawn/data/Constants.java
@@ -3,7 +3,7 @@ package com.mcmoddev.orespawn.data;
 public class Constants {
 	public static final String MODID = "orespawn";
 	public static final String NAME = "MMD OreSpawn";
-	public static final String VERSION = "3.2.0";
+	public static final String VERSION = "3.2.1";
 	public static final String RETROGEN_KEY = "Retrogen";
 	public static final String CONFIG_FILE = "config/orespawn.cfg";
 	public static final String FORCE_RETROGEN_KEY = "Force Retrogen";
