c928f3ed3f85f43580f9c7922debba8f96685675
Daniel Hazelton <dshadowwolf@gmail.com>
2018-09-02 04:44:46 -0400

    Underfluid Generator coded, not yet tested

--- src/main/java/com/mcmoddev/orespawn/data/Constants.java
+++ src/main/java/com/mcmoddev/orespawn/data/Constants.java
@@ -54,6 +54,7 @@ public class Constants {
 		public static final String ATTEMPTS_MIN = "minAttempts";
 		public static final String ATTEMPTS_MAX = "maxAttempts";
 		public static final String STARTINGFACE = "startFacing";
+		public static final String FLUID = "fluid";
 	}
 
 	public final class FileBits {
