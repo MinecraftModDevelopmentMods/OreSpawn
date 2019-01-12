4f41a4b98b34f8e0e9d35188a28cc7f9237555eb
Daniel R. Hazelton <dshadowwolf@gmail.com>
2019-01-12 11:11:45 -0500

    don't let block updates go when placing a block - it causes issues.

--- src/main/java/com/mcmoddev/orespawn/api/FeatureBase.java
+++ src/main/java/com/mcmoddev/orespawn/api/FeatureBase.java
@@ -103,7 +103,8 @@ public class FeatureBase extends IForgeRegistryEntry.Impl<IFeature> {
 			final Biome thisBiome = world.getBiome(coord);
 
 			if (replacer.test(targetBlock) && spawnData.biomeAllowed(thisBiome.getRegistryName())) {
-				world.setBlockState(coord, oreBlock);
+				// don't send block update, send to client, don't re-render, observers don't see the change
+				world.setBlockState(coord, oreBlock, 0x22);
 				return true;
 			} else {
 				return false;
