59ec8cef78ae8ab8940cadd34f4e12be74809ba3
Daniel Hazelton <dshadowwolf@gmail.com>
2019-02-09 11:39:59 -0500

    version bump in prep for push to CF to get a version with some fixes for remaining CWG bugs

--- src/main/java/com/mcmoddev/orespawn/data/Constants.java
+++ src/main/java/com/mcmoddev/orespawn/data/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 
 	public static final String MODID = "orespawn";
 	public static final String NAME = "MMD OreSpawn";
-	public static final String VERSION = "3.3.0";
+	public static final String VERSION = "3.3.1";
 	public static final String RETROGEN_KEY = "Retrogen";
 	public static final String CONFIG_FILE = "config/orespawn.cfg";
 	public static final String FORCE_RETROGEN_KEY = "Force Retrogen";
