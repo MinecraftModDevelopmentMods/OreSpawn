07ba78de9d6616acf582ddcc8a4fa2059d4be522
Daniel Hazelton <dshadowwolf@gmail.com>
2019-03-03 19:58:55 -0500

    ...how the fracking hell did I forget this?

---
+++ src/main/java/com/mcmoddev/orespawn/api/exceptions/BadStateValueException.java
@@ -0,0 +1,13 @@
+package com.mcmoddev.orespawn.api.exceptions;
+
+public class BadStateValueException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3826628238012469423L;
+
+	public BadStateValueException(String msg) {
+		super(msg);
+	}
+}
