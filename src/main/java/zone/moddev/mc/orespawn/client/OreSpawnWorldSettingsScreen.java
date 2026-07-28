b6daa2f21c4233cde8767e05485d40a8f1b3f4cf
SkyBlase1978 <johnbraham1978@outlook.com>
2026-07-28 16:19:41 +0100

    fixes issue where mineralogy would cause springs not to spawn

--- src/main/java/zone/moddev/mc/orespawn/client/OreSpawnWorldSettingsScreen.java
+++ src/main/java/zone/moddev/mc/orespawn/client/OreSpawnWorldSettingsScreen.java
@@ -67,6 +67,7 @@ public final class OreSpawnWorldSettingsScreen extends Screen {
 
 	@Override
 	protected void init() {
+		clearWidgetReferences();
 		OreSpawnScreenLayout.beginHelp(this);
 		int contentWidth = Math.min(390, Math.max(310, this.width - 24));
 		columnWidth = (contentWidth - 5) / 2;
@@ -142,7 +143,7 @@ public final class OreSpawnWorldSettingsScreen extends Screen {
 			formationContinuityButton = addPresetButton(left, top + (row * rowIndex),
 					"option.orespawn.formation_continuity", "tooltip.orespawn.formation_continuity",
 					formationContinuity, value -> formationContinuity = value);
-			if (vanillaOresButton == null) vanillaOresButton = addVanillaOresButton(right,
+			if (fluids) vanillaOresButton = addVanillaOresButton(right,
 					top + (row * rowIndex), columnWidth);
 			rowIndex++;
 			addRenderableWidget(OreSpawnScreenLayout.explainedButton(this, font, left, top + (row * rowIndex),
@@ -167,6 +168,17 @@ public final class OreSpawnWorldSettingsScreen extends Screen {
 		addRenderableWidget(OreSpawnScreenLayout.button(this, font, right, this.height - 28, columnWidth, BUTTON_HEIGHT,
 				CommonComponents.GUI_CANCEL, button -> onClose()));
 		updateFormationControls();
+	}
+
+	private void clearWidgetReferences() {
+		geologyModeButton = null;
+		fluidDepositsButton = null;
+		vanillaOresButton = null;
+		horizontalSizeButton = null;
+		verticalThicknessButton = null;
+		wavinessButton = null;
+		edgeIrregularityButton = null;
+		formationContinuityButton = null;
 	}
 
 	private CycleButton<Boolean> addVanillaOresButton(int x, int y, int width) {
