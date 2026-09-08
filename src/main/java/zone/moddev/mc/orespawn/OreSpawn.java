f104e17c0f7345e8263d97c8b58e5d5f2f8e3582
JohnBraham <johnbraham1978@outlook.com>
2026-09-08 22:38:22 +0100

    Fix Stable Layers rock height eligibility

--- src/main/java/zone/moddev/mc/orespawn/OreSpawn.java
+++ src/main/java/zone/moddev/mc/orespawn/OreSpawn.java
@@ -52,7 +52,7 @@ public class OreSpawn {
 
 	public static final String MODID = "orespawn";
 	public static final String NAME = "OreSpawn";
-	public static final String VERSION = "4.0.9.110021";
+	public static final String VERSION = "4.0.10.110021";
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
